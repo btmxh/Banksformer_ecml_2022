f61e2f68c2ab44bbb25de9640f65695ff060c941
btmxh <ngoduyanh.chip@gmail.com>
2025-05-05 12:57:59 +0700

    Add `clear_old.sh`

---
+++ uk/banksformer/clear_old.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+
+# %%
+rm -r checkpoints/
+
+# %%
+rm -r stored_data/
+
+# %%
+rm -r generated_data/
+
+# %%
+rm -r generation_results/
+
+# %%
+rm -r training_history
+
+# %%
+
+# %%
+mkdir checkpoints/
+
+# %%
+mkdir stored_data/
+
+# %%
+mkdir generated_data/
+
+# %%
+mkdir generation_results/
+
+# %%
+mkdir training_history
+
+# %%
